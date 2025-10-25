a04b86694145ec89b8ce798ac976b0a895d89e6b
SleepY010221 <santiagoadi0101@gmail.com>
2025-10-25 16:07:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const path = require('path');
 const db = require('./db');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve frontend
+// Serve frontend static files
 app.use(express.static(path.join(__dirname, 'frontend')));
-res.sendFile(path.join(__dirname, 'frontend/index.html'));
+
+// Root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/index.html'));
 });
 
 // Register
@@ -96,5 +100,7 @@ app.delete('/api/expenses/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
